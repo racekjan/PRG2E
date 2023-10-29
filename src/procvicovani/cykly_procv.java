9b62ed57e94d09c5179845eff50123a57ba431d7
Jan Racek <janra2323@gmail.com>
2023-10-29 13:17:49 +0100

    procvicovani krome posledniho prikladu

---
+++ src/procvicovani/cykly_procv.java
@@ -0,0 +1,59 @@
+package procvicovani;
+
+import java.util.Scanner;
+
+public class cykly_procv {
+    public static void main(String[] args) {
+//        Scanner sc = new Scanner(System.in);
+//        int input = sc.nextInt();
+//        int soucet = 0;
+//
+//        while (input != -1){
+//            soucet = soucet + input;
+//            input = sc.nextInt();
+//        }
+//        System.out.println(soucet);
+
+//        Scanner sc = new Scanner(System.in);
+//        int input = sc.nextInt();
+//        int vysledek = 1;
+//        for (int i = 1; i <= input; i++) {
+//            vysledek = vysledek * i;
+//        }
+//        System.out.println(vysledek);
+
+//        Scanner sc = new Scanner(System.in);
+//        System.out.println("Zadej cislo");
+//        int cislo = sc.nextInt();
+//        System.out.println("Zadej mocninu");
+//        int mocnina = sc.nextInt();
+//        int vysledek = cislo;
+//        for (int i = 1; i < mocnina; i++) {
+//            vysledek = vysledek*cislo;
+//        }
+//        System.out.println(vysledek);
+
+//        int random = (int)(Math.random()*100 + 1);
+//        Scanner sc = new Scanner(System.in);
+//        int tip = sc.nextInt();
+//        while (tip != random){
+//            if (tip>random){
+//                System.out.println("Moc velke cislo");
+//            }else {
+//                System.out.println("Moc male cislo");
+//            }
+//            tip = sc.nextInt();
+//        }
+
+//        int pocet = 0;
+//        for (int i = 1; i <= 1000000; i++) {
+//            int random = (int)(Math.random()*100+1);
+//            if (random==42){
+//                pocet++;
+//            }
+//        }
+//        System.out.println("Cislo 42 padlo " + pocet + "x");
+
+
+    }
+}
