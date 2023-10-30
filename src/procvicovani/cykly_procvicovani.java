503fccdae59e4a78b9f5ccd3e31c0f69fd30409d
racekjan <janra2323@gmail.com>
2023-10-30 10:35:33 +0100

    cykly procv

---
+++ src/procvicovani/cykly_procvicovani.java
@@ -0,0 +1,72 @@
+package procvicovani;
+
+import java.util.Scanner;
+
+public class cykly_procvicovani {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        int cisla = sc.nextInt();
+//        int vysledek = 0;
+//
+//        while (cisla!=-1){
+//            vysledek = vysledek + cisla;
+//            cisla = sc.nextInt();
+//        }
+//        System.out.println(vysledek);
+
+//        int cislo = sc.nextInt();
+//        int vysledek = 1;
+//        for (int i = 1; i <= cislo; i++) {
+//            vysledek = vysledek * i;
+//        }
+//        System.out.println(vysledek);
+
+//        System.out.println("Zadej cislo");
+//        int cislo = sc.nextInt();
+//        System.out.println("Zadej mocninu");
+//        int mocnina = sc.nextInt();
+//        int vysledek = 1;
+//
+//        for (int i = 1; i <= mocnina; i++) {
+//            vysledek = vysledek*cislo;
+//        }
+//        System.out.println(vysledek);
+
+//        int cislo = (int)(Math.random()*100+1);
+//        int tip = sc.nextInt();
+//        while (tip!=cislo){
+//            if (tip>cislo){
+//                System.out.println("Moc velke cislo");
+//            }else {
+//                System.out.println("Moc male cislo");
+//            }
+//            tip = sc.nextInt();
+//        }
+
+
+//        int pocet = 0;
+//        for (int i = 1; i < 100000; i++) {
+//            int random = (int)(Math.random()*100+1);
+//            if (random == 42){
+//                pocet++;
+//            }
+//        }
+//        System.out.println("Cislo 42 padlo " + pocet + "x");
+
+//        int pocet =  sc.nextInt();
+//        for (int i = 1; i < pocet*pocet+1; i++) {
+//            System.out.print(i + "\t");
+//            if (i % pocet == 0){
+//                System.out.println();
+//            }
+//        }
+
+
+
+
+
+
+
+
+    }
+}
