9ad272be9bce6e3cd5e989732e3a3b689f71c973
jan.racek <janra2323@gmail.com>
2023-11-14 08:45:24 +0100

    sort pole

--- src/pole/ArraysBasic.java
+++ src/pole/ArraysBasic.java
@@ -3,7 +3,7 @@ package pole;
 public class ArraysBasic {
     public static void main(String[] args) {
             //staticke vytvoreni pole
-//            int[] pole = {45, 125, 19, 25, 27, 42};
+            int[] pole = {45, 125, 19, 25, 27, 42};
 //            System.out.println(pole[4]);
 
             int[] dalsiPole = new int[10];
@@ -18,6 +18,17 @@ public class ArraysBasic {
                 System.out.print(dalsiPole[i] + " ");
             }
 
+            int[] switches = {5,6,9};
+            int temp = switches[0];
+            switches[0]=switches[2];
+            switches[2]=temp;
+
+        for (int i = 0; i < pole.length -1; i++) {
+            if (pole[i] > pole[i+1]) {
+                System.out.println("Neni serazeno");
+                break;
+            }
+        }
             
     }
 }
