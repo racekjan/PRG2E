caa510b3869a799d28751cc082bd4de82a862189
jan.racek <janra2323@gmail.com>
2023-12-18 11:21:36 +0100

    test strings

---
+++ src/testy/test_retezce.java
@@ -0,0 +1,93 @@
+package testy;
+
+import java.util.Scanner;
+
+public class test_retezce {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+//        String[] jmena = {"Karel", "Hynek", "Jaromir"};
+//        String[] prijmeni = {"Macha", "Erben", "Novak"};
+//        int randomJmeno = (int)(Math.random()*jmena.length);
+//        int randomPrijmeni = (int)(Math.random()* prijmeni.length);
+//        System.out.println(jmena[randomJmeno] + " " + prijmeni[randomPrijmeni]);
+
+//        String heslo = sc.nextLine();
+//        boolean ok = false;
+//        char[] hesloArray = heslo.toCharArray();
+//        while (ok==false) {
+//            hesloArray = heslo.toCharArray();
+//            if (hesloArray.length == 8) {
+//                if (heslo.charAt(0)=='H'){
+//                    if (heslo.indexOf('*')>=0){
+//                        ok = true;
+//                    }
+//                }
+//            }
+//            if (ok==false){
+//                System.out.println("zkus to znovu");
+//                heslo=sc.nextLine();
+//            }
+//        }
+//        System.out.println("Heslo " + heslo + " je OK");
+
+//        String neco = "dsaudhbadcjsahbca.dsajckdsac!dsahcjdhs?";
+//        char[] necoArray = neco.toCharArray();
+//        int counter = 0;
+//        for (int i = 0; i < necoArray.length; i++) {
+//            if (necoArray[i]=='!' || necoArray[i]=='.' || necoArray[i]=='?'){
+//                counter++;
+//            }
+//        }
+//        System.out.println(counter);
+
+//        String cislo = sc.nextLine();
+//        char[] cisloArray = cislo.toCharArray();
+//        if (cislo.contains("+420") && cisloArray.length==13){
+//            System.out.println("Cislo je Ok");
+//        }
+
+//        String cislo = sc.nextLine();
+//        char[] cisloArray = cislo.toCharArray();
+//        boolean ok = false;
+//        while (ok==false) {
+//            if (cisloArray.length == 13 && cislo.matches("[+][4][2][0][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9][0-9]")) {
+//                System.out.println("Cislo je Ok");
+//                ok = true;
+//            }else {
+//                System.out.println("Zkus to znovu");
+//                cislo = sc.nextLine();
+//            }
+//        }
+
+//        String[] spanishPhrases = {"El camino", "La Bomba", "El Pistolero", "Elephant", "Lancelot"};
+//        for (int i = 0; i < spanishPhrases.length; i++) {
+//            if (spanishPhrases[i].contains("El ") || spanishPhrases[i].contains("La ") || spanishPhrases[i].contains("A ")){
+//                System.out.println(spanishPhrases[i]);
+//            }
+//        }
+
+//        String[] hodnoty = {"djhcsahjc","dhksadkhsa", "jdsakjdsa"};
+//        int konec = hodnoty.length-1;
+//        for (int i = 0; i < hodnoty.length; i++) {
+//            if (i==0 || i ==konec){
+//                System.out.println(hodnoty[i].toUpperCase());
+//            }else {
+//                System.out.println(hodnoty[i].toLowerCase());
+//            }
+//        }
+
+//        String text = "dzsadhbsadjhf*gbfdgfd*bgbfd*bg*fdbgfd*bg*fdbg";
+//        char[] textArray = text.toCharArray();
+//        int counter = 0;
+//        for (int i = 0; i < textArray.length; i++) {
+//            if (textArray[i] == '*'){
+//                counter++;
+//            }
+//        }
+//        System.out.println("* se vyskytuje " + counter + " krat");
+
+
+
+
+    }
+}
