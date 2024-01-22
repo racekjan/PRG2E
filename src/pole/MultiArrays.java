6ed7df78659bf052fdc98472800ea2bfd583c3de
jan.racek <janra2323@gmail.com>
2024-01-22 11:24:22 +0100

    2d pole

---
+++ src/pole/MultiArrays.java
@@ -0,0 +1,22 @@
+package pole;
+
+public class MultiArrays {
+    public static void main(String[] args) {
+        int[] numbers = {1,2,3};
+        int[] simpleArrays = new int[5];
+
+        int[][] twoArrays = {
+                {5,6,8},
+                {5,2,3},
+                {5,1,6}
+        };
+        System.out.println(twoArrays[2][1]);
+        int[][] arrays = new int[3][5];
+
+        int[][] uglyTwoArrays = {{5,6,8}, {5,2,3}, {5,1,6}};
+
+
+
+
+    }
+}
