82ac722790c87ead12ebbb0b29fce064cc64ffae
jan.racek <janra2323@gmail.com>
2024-03-18 11:20:23 +0100

    oop

---
+++ src/oop/Osoba.java
@@ -0,0 +1,14 @@
+package oop;
+
+public class Osoba {
+    //atributy
+    String name;
+    int age;
+    double shoeSize;
+
+    //metody
+    public void intro (){
+        System.out.println("Hi, I'm " + name + " and I'm " + age);
+    }
+
+}
---
+++ src/oop/PersonTest.java
@@ -0,0 +1,19 @@
+package oop;
+
+import java.math.BigInteger;
+
+public class PersonTest {
+    public static void main(String[] args) {
+        Osoba person = new Osoba();
+        person.age = 21;
+        person.name = "Karel";
+        person.shoeSize = 47.75;
+        person.intro();
+        Osoba otherPerson = new Osoba();
+        otherPerson.age = 30;
+        otherPerson.name = "Pepik";
+        otherPerson.shoeSize = 36.5;
+        otherPerson.intro();
+
+    }
+}
