e8b69fc2238e12b6d2a00e78cc23169dceb8c158
jan.racek <janra2323@gmail.com>
2024-05-28 08:45:30 +0200

    bludiste finished

--- src/oop/maze/Maze.java
+++ src/oop/maze/Maze.java
@@ -1,5 +1,7 @@
 package oop.maze;
 
+import com.sun.source.tree.BreakTree;
+
 public class Maze {
     int[][] mazeArray;
     char wall, playerIcon, freeSpace, treasure;
@@ -15,7 +17,11 @@ public class Maze {
     void printMaze(){
         for (int i = 0; i < mazeArray.length; i++) {
             for (int j = 0; j < mazeArray[i].length; j++) {
-                System.out.print(getCharacter(mazeArray[i][j]));
+                if (i == player.x && j == player.y){
+                    System.out.print(playerIcon);
+                }else {
+                    System.out.print(getCharacter(mazeArray[i][j]));
+                }
             }
             System.out.println();
         }
@@ -25,10 +31,28 @@ public class Maze {
             case 0: return freeSpace;
             case 1: return wall;
             case 2: return playerIcon;
-            case 3: return  treasure;
+            case 3: return treasure;
         }
         return 'x';
     }
+
+    void game(){
+        printMaze();
+        while (mazeArray[player.x][player.y] != 3){
+            player.readMove();
+            printMaze();
+            if (hitWall()){
+                System.out.println("Narazil jsi do zdi, hra je ukoncena");
+                return;
+            }
+        }
+        System.out.println("Nasel jsi poklad, hra je ukoncena..");
+    }
+
+    boolean hitWall(){
+        return mazeArray[player.x][player.y] == 1;
+    }
+
 
     public static void main(String[] args) {
         int[][] mazeArray= {
@@ -36,13 +60,13 @@ public class Maze {
             {1,0,0,0,0,0,1},
             {1,0,1,1,1,0,1},
             {1,0,1,0,0,0,1},
-            {1,2,1,3,1,0,1},
+            {1,0,1,3,1,0,1},
             {1,1,1,1,1,1,1},
         };
-        Player p = new Player("John");
+        Player p = new Player("John",4,1);
 
         Maze m = new Maze(mazeArray, '#','O',' ', '+', p);
-        m.printMaze();
+        m.game();
     }
 
 
--- src/oop/maze/Player.java
+++ src/oop/maze/Player.java
@@ -4,14 +4,27 @@ import java.util.Scanner;
 
 public class Player {
     String nickname;
+    int x,y;
 
-    public Player(String nickname) {
+    public Player(String nickname, int x, int y) {
         this.nickname = nickname;
+        this.x = x;
+        this.y = y;
     }
 
     String readMove(){
         System.out.println("Vyber si smer (a,w,s,d): ");
         Scanner sc = new Scanner(System.in);
-        return sc.nextLine();
+        String input = sc.nextLine();
+        switch (input){
+            case "a": y--; break;
+            case "w": x--; break;
+            case "s": x++; break;
+            case "d": y++; break;
+            default:
+                System.out.println("Spatny input");
+                return "X";
+        }
+        return input;
     }
 }
